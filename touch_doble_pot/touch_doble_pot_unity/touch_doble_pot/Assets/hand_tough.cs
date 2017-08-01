83f1724e60b4767484b2fdd90236643f2c967c31
PeterLukGit <peter33633@gmail.com>
2017-08-02 00:17:37 +0800

    20170801
    手機觸碰+ test_Z

--- touch_doble_pot/touch_doble_pot_unity/touch_doble_pot/Assets/hand_tough.cs
+++ touch_doble_pot/touch_doble_pot_unity/touch_doble_pot/Assets/hand_tough.cs
@@ -6,7 +6,7 @@ public class hand_tough : MonoBehaviour
 {
 
     //public enum gesture {left,right,up,down }
-    
+
     //point物件
     [Header("point物件(攝影機父物件)")]
     public GameObject point;
@@ -55,10 +55,46 @@ public class hand_tough : MonoBehaviour
     [Range(0f, 10f)]
     public float move_point_time_max = 1f;
 
+    //攝影機最遠位置
+    [Header("攝影機最遠位置")]
+    [Range(0f,50f)]
+    public float camera_up = 20f;
+
+    //攝影機最近位置
+    [Header("攝影機最近位置")]
+    [Range( 0f,50f)]
+    public float camera_down = 10f;
+
+    //攝影機原始位置
+    [Header("攝影機原始位置")]
+    public Vector3 camera_reduction_vec3;
+
+    //攝影機父物件原始位置
+    [Header("攝影機父物件原始位置")]
+    public Vector3 point_reduction_vec3;
+
+    //攝影機原始旋轉
+    [Header("攝影機原始旋轉")]
+    public Quaternion camera_reduction_quater;
+
+    //攝影機父物件原始旋轉
+    [Header("攝影機父物件原始旋轉")]
+    public Quaternion point_reduction_quater;
+
+    //還原開關待刪除
+    [Header("還原開關待刪除")]
+    public bool reduction = false;
+
     // Use this for initialization
     void Start()
     {
         move_point_time = move_point_time_max;
+
+        point_reduction_vec3 = point.transform.position;
+        point_reduction_quater = point.transform.rotation;
+
+        camera_reduction_vec3 = camera_gam.transform.position;
+        camera_reduction_quater = camera_gam.transform.rotation;
 
     }
 
@@ -70,6 +106,13 @@ public class hand_tough : MonoBehaviour
         mobile_input();
         //point.transform.Rotate(x,y,z, Space.World);
         //point.transform.rotation = Quaternion.Euler(x, y, z);
+
+        //還原待刪除
+        if(reduction == true)
+        {
+            point_camera_reduction();
+            reduction = false;
+        }
     }
 
     void mobile_input()
@@ -88,7 +131,7 @@ public class hand_tough : MonoBehaviour
             //開始觸碰
             if (Input.touches[0].phase == TouchPhase.Began)
             {
-                print("點擊");
+                print("一根點擊");
                 //print(Input.touches[0].position + " " + Input.touches[0].deltaPosition);
                 //Input.touches[0].position ==> 手指位置  Input.touches[0].deltaPosition ==>手指移動位置(手指位移方位)
                 one_finger_pos = Input.touches[0].position;
@@ -96,12 +139,12 @@ public class hand_tough : MonoBehaviour
             }
             else if ((Input.touches[0].phase == TouchPhase.Stationary) && (first_chack == true))//觸碰不移動
             {
-                print("停留");
+                print("一根停留");
                 time_move_point();
             }
             else if (Input.touches[0].phase == TouchPhase.Moved)//移動
             {
-                print("移動");
+                print("一根移動");
 
                 print(Input.touches[0].position + " " + Input.touches[0].deltaPosition);
 
@@ -140,7 +183,7 @@ public class hand_tough : MonoBehaviour
                         //point.transform.Rotate(one_finger_displacement.y * rotate_speed * Time.deltaTime, 0, 0, Space.World);
                         point.transform.rotation = Quaternion.Euler(
                             point.transform.rotation.eulerAngles.x + one_finger_displacement.y * rotate_speed * Time.deltaTime,
-                            point.transform.rotation.eulerAngles.y, 
+                            point.transform.rotation.eulerAngles.y,
                             0);
                     }
 
@@ -164,22 +207,124 @@ public class hand_tough : MonoBehaviour
         else if (Input.touchCount >= 2)//手指2根時
         {
             print("二根");
+
+            //記錄兩個手指位置
+            Vector2 finger1 = new Vector2();
+            Vector2 finger2 = new Vector2();
+
+            //記錄兩個手指移動距離
+            Vector2 move1 = new Vector2();
+            Vector2 move2 = new Vector2();
+
+            //是否是小於2點觸碰
+            for (int i = 0; i < 2; i++)
+            {
+                Touch touch = Input.touches[i];
+                //Input.touches 触摸列表 static var touches : Touch[]
+
+
+
+                if (touch.phase == TouchPhase.Ended)
+                    break;
+
+                if (touch.phase == TouchPhase.Moved)
+                {
+                    //每次都重置
+                    float move = 0;
+
+                    //觸碰一點
+                    if (i == 0)
+                    {
+                        finger1 = touch.position;
+                        move1 = touch.deltaPosition;
+                        //另一點
+                    }
+                    else
+                    {
+                        finger2 = touch.position;
+                        move2 = touch.deltaPosition;
+
+                        //取最大X
+                        if (finger1.x > finger2.x)
+                        {
+                            move = move1.x;
+                            //print(move);
+                        }
+                        else
+                        {
+                            move = move2.x;
+                            //print(move);
+                        }
+
+                        //取最大Y，並與取出的X累加
+                        if (finger1.y > finger2.y)
+                        {
+                            move += move1.y;
+                            //print(move);
+                        }
+                        else
+                        {
+                            move += move2.y;
+                            //print(move);
+                        }
+
+                        //只拿取最大點的位移，例X最大點位移為正就放大，負為縮
+
+                        //print(finger1 + " " + finger2 + " " + move1 + " " + move2 + " " + move);
+                        //當兩指距離越遠，Z位置加的越多，相反之
+                        //Camera.main.transform.Translate(0, 0, move * Time.deltaTime);
+
+                        if ((Vector3.Distance(point.transform.position , camera_gam.transform.position) - move * Time.deltaTime >= camera_down) && (Vector3.Distance(point.transform.position, camera_gam.transform.position) - move* Time.deltaTime <= camera_up))
+                        {
+                            camera_gam.transform.Translate(0, 0, move * Time.deltaTime);
+                        }
+                        //else
+                        //{
+                        //    if (move > 0)
+                        //    {
+                        //        camera_gam.transform.position = new Vector3(
+                        //            camera_gam.transform.position.x,
+                        //            camera_gam.transform.position.y,
+                        //            camera_down);
+                        //    }
+                        //    else
+                        //    {
+                        //        camera_gam.transform.position = new Vector3(
+                        //            camera_gam.transform.position.x,
+                        //            camera_gam.transform.position.y,
+                        //            camera_up);
+                        //    }
+                        //}
+
+                    }
+                }
+
+            }
         }
     }
 
-
     void time_move_point()
     {
-        if(move_point_time <= move_point_time_max)
+        if (move_point_time <= move_point_time_max)
         {
             move_point_time -= Time.deltaTime;
-            if(move_point_time <= 0)
+            if (move_point_time <= 0)
             {
                 move_point_time = move_point_time_max;
                 move_point = true;
                 first_chack = false;
             }
         }
+    }
+
+    void point_camera_reduction()
+    {
+        point.transform.position = point_reduction_vec3;
+        point.transform.rotation = point_reduction_quater;
+
+        camera_gam.transform.position = camera_reduction_vec3;
+        camera_gam.transform.rotation = camera_reduction_quater;
+
     }
 
 }
---
+++ touch_doble_pot/touch_doble_pot_unity/touch_doble_pot/Assets/test_Z.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_Z : MonoBehaviour {
+
+    /*
+ //   public GameObject aaa;
+
+    //   public Vector3 acemra;
+
+    //   public Ray rayss;
+
+    //   public float x;
+    //   public float y;
+    //   public Vector3 targetposition;
+
+    //   // Use this for initialization
+    //   void Start () {
+
+    //}
+
+    //// Update is called once per frame
+    //void Update () {
+    //       //acemra = Camera.main.ScreenToWorldPoint(new Vector3( Input.mousePosition.x,Input.mousePosition.y,Input.mousePosition.z));
+    //       ////print(Input.mousePosition);
+    //       //print(acemra);
+
+    //       if (Input.GetMouseButtonDown(0))
+    //       {
+    //           x = Input.mousePosition.x;
+    //           y = Input.mousePosition.y;
+
+
+    //       }
+
+    //       targetposition = Camera.main.ScreenToWorldPoint(new Vector3(x, 1.1f, y));
+    //       aaa.transform.position = Vector3.MoveTowards(transform.position, targetposition, Time.deltaTime * 2);
+
+    //   }
+    */
+
+    //你現在的問題是要把螢幕2D的滑鼠座標(x , y , 0 )
+    //轉換成滑鼠在那一點時的空間座標
+
+    //先把你的人物的Transform放入 character
+    //var character : Transform;   
+    //創建一個相機
+    //var mainCamera : Camera;    
+    public Camera mainCamera;
+
+    public Transform character;
+
+    //然後做一個平面
+    //var playerMovementPlane : Plane;    
+
+    public Plane playerMovementPlane;
+
+    public Vector3 cursorScreenPosition;
+
+    public Vector3 cursorWorldPosition;
+
+    public GameObject cube;
+
+    private void Awake()
+    {
+        //        然後把mainCamera設為主相機
+        //mainCamera = Camera.main;
+
+        //playerMovementPlane = new Plane(character.up, character.position);
+    }
+
+    public float aaa;
+
+    private void Update()
+    {
+        //        宣告螢幕2D的滑鼠質點
+        //        var cursorScreenPosition: Vector3 = Input.mousePosition;
+        //        宣告空間中的滑鼠質點 這裡要進行一些轉換就可以得到了  mainCamera是你的主相機
+        //       var cursorWorldPosition: Vector3 = ScreenPointToWorldPointOnPlane(cursorScreenPosition, playerMovementPlane, mainCamera);
+
+        playerMovementPlane = new Plane(character.up, character.position);
+        //playerMovementPlane = new Plane(character.up, aaa);
+        //playerMovementPlane = new Plane(new Vector3(0,0,0), new Vector3(0,0,1), new Vector3(1,0,1));
+
+        cursorScreenPosition = Input.mousePosition;//滑鼠
+
+        //cursorScreenPosition = Input.touches[0].position;//手指
+
+        cursorWorldPosition = ScreenPointToWorldPointOnPlane(cursorScreenPosition, playerMovementPlane, mainCamera);
+        print(cursorWorldPosition);
+        cube.transform.position = cursorWorldPosition;
+    }
+
+    //function Awake()
+    //    {
+    //        然後把mainCamera設為主相機
+    //        mainCamera = Camera.main;
+
+    //        然後把NEW出這個平面並且把這個平面放到角色上
+    //        playerMovementPlane = new Plane(character.up, character.position);
+    //    }
+    //    function Update()
+    //    {
+    //        宣告螢幕2D的滑鼠質點
+    //        var cursorScreenPosition: Vector3 = Input.mousePosition;
+    //        宣告空間中的滑鼠質點 這裡要進行一些轉換就可以得到了  mainCamera是你的主相機
+    //       var cursorWorldPosition: Vector3 = ScreenPointToWorldPointOnPlane(cursorScreenPosition, playerMovementPlane, mainCamera);
+    //    }
+
+    public static Vector3 ScreenPointToWorldPointOnPlane(Vector3 screenPoint, Plane plane, Camera camera)
+    {
+        //將滑鼠的螢幕位置轉換成空間中的射線 ray=射線   
+        //    var ray : Ray = camera.ScreenPointToRay(screenPoint);    
+        Ray ray = camera.ScreenPointToRay(screenPoint);
+        //    //找出射線與平面相交   
+        //    return PlaneRayIntersection(plane, ray);
+        Debug.DrawRay(ray.origin, ray.direction);
+
+        return PlaneRayIntersection(plane, ray);
+    }
+
+    //    public static function ScreenPointToWorldPointOnPlane(screenPoint : Vector3, plane : Plane, camera : Camera) : Vector3 {    
+    //    //將滑鼠的螢幕位置轉換成空間中的射線 ray=射線   
+    //    var ray : Ray = camera.ScreenPointToRay(screenPoint);    
+    //    //找出射線與平面相交   
+    //    return PlaneRayIntersection(plane, ray);
+    //}
+    public static Vector3 PlaneRayIntersection(Plane plane, Ray ray)
+    {
+        //    var dist : float;    
+        //    //光线投射 一条射线和平面相交。   
+        //    plane.Raycast(ray, dist);    
+        //    //获取点 返回沿着射线在distance距离单位的点。   
+        //    return ray.GetPoint(dist);    
+        float dist;
+        plane.Raycast(ray,out dist);
+        print(dist);
+        return ray.GetPoint(dist);
+    }
+
+    //public static function PlaneRayIntersection(plane : Plane, ray : Ray) : Vector3 {    
+    //    var dist : float;    
+    //    //光线投射 一条射线和平面相交。   
+    //    plane.Raycast(ray, dist);    
+    //    //获取点 返回沿着射线在distance距离单位的点。   
+    //    return ray.GetPoint(dist);    
+    //}    
+
+
+}
