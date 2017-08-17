dfc0c6b84c2298c98002d379b8981a802bc13ee4
PeterLukGit <peter33633@gmail.com>
2017-08-18 00:11:02 +0800

    20170817

---
+++ touch_prop_Integration_2/touch_prop_Integration_unity/touch_prop_Integration_2/Assets/z_range_draw.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class z_range_draw : MonoBehaviour
+{
+
+    public GameObject game_1;
+
+    public GameObject game_2;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawLine(game_1.transform.position, new Vector3(game_2.transform.position.x, game_1.transform.position.y, game_1.transform.position.z),Color.black);
+        Debug.DrawLine(game_1.transform.position, new Vector3(game_1.transform.position.x, game_2.transform.position.y, game_1.transform.position.z), Color.black);
+        Debug.DrawLine(game_2.transform.position, new Vector3(game_1.transform.position.x, game_2.transform.position.y, game_1.transform.position.z), Color.black);
+        Debug.DrawLine(game_2.transform.position, new Vector3(game_2.transform.position.x, game_1.transform.position.y, game_1.transform.position.z), Color.black);
+    }
+}
