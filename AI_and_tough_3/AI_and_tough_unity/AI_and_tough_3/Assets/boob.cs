e801364998c288daab7073e6277fff2f9bd9c84b
PeterLukGit <peter33633@gmail.com>
2017-10-03 23:30:21 +0800

    20171003

---
+++ AI_and_tough_3/AI_and_tough_unity/AI_and_tough_3/Assets/boob.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VoxelMax;
+
+public class boob : MonoBehaviour {
+
+    public float time = 1f;
+
+    public VoxelBomb _VoxelBomb;
+
+    // Use this for initialization
+    void Start ()
+    {
+        _VoxelBomb.triggered = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        time -= Time.deltaTime;
+
+        if(time <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
