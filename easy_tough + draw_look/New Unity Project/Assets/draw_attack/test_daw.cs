09f3782c4971a36be28df8b59ad51593f44020e9
PeterLukGit <peter33633@gmail.com>
2017-10-15 20:41:33 +0800

    20171015

---
+++ easy_tough + draw_look/New Unity Project/Assets/draw_attack/test_daw.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_daw : MonoBehaviour {
+
+    public float aaa,bbb;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        DrawTool.DrawSector(transform, transform.localPosition, aaa, bbb);
+        //DrawTool.DrawSectorSolid(transform, transform.localPosition, aaa, bbb);
+    }
+}
