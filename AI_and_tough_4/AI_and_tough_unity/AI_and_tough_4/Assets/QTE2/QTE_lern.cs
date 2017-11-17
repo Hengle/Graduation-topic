bc964f7f102d3ff81cb823dd13c77ef8ba02fe4a
PeterLukGit <peter33633@gmail.com>
2017-11-18 01:16:11 +0800

    20171117

--- AI_and_tough_4/AI_and_tough_unity/AI_and_tough_4/Assets/QTE2/QTE_lern.cs
+++ AI_and_tough_4/AI_and_tough_unity/AI_and_tough_4/Assets/QTE2/QTE_lern.cs
@@ -11,13 +11,15 @@ public class QTE_lern : MonoBehaviour
 
     //public AnimationCurve[] target_lern_Premium;
 
-    public float time;
+    //public float time;
 
     public int i;
 
     public GameObject target;
 
     public float target_num;
+
+    public float long_y;
 
     // Use this for initialization
     void Start()
@@ -28,7 +30,7 @@ public class QTE_lern : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        print(target_lern_basic[1].Evaluate(time));
-        target.transform.localPosition =  new Vector3(0,400 +( target_lern_basic[i].Evaluate(Time.time)-0.5f) * target_num, 0);
+        //print(target_lern_basic[1].Evaluate(time));
+        target.transform.localPosition = new Vector3(0, long_y + ( target_lern_basic[i].Evaluate(Time.time)) * target_num, 0);
     }
 }
---
+++ AI_and_tough_4/AI_and_tough_unity/AI_and_tough_4/Assets/QTE2/QTE_target_z.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QTE_target_z : MonoBehaviour {
+
+    public AnimationCurve[] target_z_lern_basic;
+
+    public int i;
+
+    public GameObject target;
+
+    public float target_num;
+
+    public Quaternion angle;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //target.transform.localRotation = new Vector3(0,0, (target_z_lern_basic[i].Evaluate(Time.time)) * target_num);
+        angle.eulerAngles = new Vector3(0, 0, (target_z_lern_basic[i].Evaluate(Time.time) - 0.5f) * target_num);
+        target.transform.localRotation = angle;
+
+    }
+}
