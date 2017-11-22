b6ba1881d91bf18e7fa080da8dc23bdf915af57b
PeterLukGit <peter33633@gmail.com>
2017-11-23 00:43:29 +0800

    20171123

---
+++ AI_and_tough_4/AI_and_tough_unity/AI_and_tough_4/Assets/eye_mesh_test.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eye_mesh_test : MonoBehaviour
+{
+
+    public LayerMask layer;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == Mathf.Log(layer, 2))
+        {
+            print(other.gameObject + "  " + Time.time);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.layer == Mathf.Log(layer,2))
+        {
+            print(other.gameObject + "  " + Time.time);
+        }
+    }
+
+}
