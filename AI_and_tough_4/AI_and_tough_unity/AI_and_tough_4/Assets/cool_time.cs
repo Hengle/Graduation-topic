c44f40b3f0d1a6607084cd2e56280201233fe464
PeterLukGit <peter33633@gmail.com>
2017-12-12 00:16:28 +0800

    20171211

---
+++ AI_and_tough_4/AI_and_tough_unity/AI_and_tough_4/Assets/cool_time.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cool_time : MonoBehaviour
+{
+
+    public Toggle toggle_ui;
+
+    public float time;
+
+    public float time_cool;
+
+    public bool cool_on;
+
+    //public float cool_image_float;
+
+    public Image cool_image;
+
+    // Use this for initialization
+    void Start()
+    {
+        time = time_cool;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cool_on == true)
+        {
+            Invasionlight_cool();
+        }
+    }
+
+    void Invasionlight_cool()
+    {
+        if(time > 0)
+        {
+            time -= Time.deltaTime;
+            cool_image.fillAmount = ((time_cool - time) / time_cool);
+            toggle_ui.interactable = false;
+        }
+        else
+        {
+            time = time_cool;
+            cool_image.fillAmount = 1;
+            toggle_ui.interactable = true;
+            cool_on = false;
+        }
+    }
+
+    public void cool()
+    {
+        time = time_cool;
+        cool_image.fillAmount = 0;
+        toggle_ui.interactable = false;
+        cool_on = true;
+    }
+
+}
---
+++ AI_and_tough_4/AI_and_tough_unity/AI_and_tough_4/Assets/event_prop_cool.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class event_prop_cool : MonoBehaviour
+{
+
+    public cool_time _cool_Time;
+
+
+    private void Awake()
+    {
+        _cool_Time = (cool_time)FindObjectOfType(typeof(cool_time));
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        _cool_Time.cool();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
